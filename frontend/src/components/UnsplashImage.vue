5f512b179901ded8d200924a1b77778a1fa9ec69
David Pratt <david@davideugenepratt.com>
2020-04-13 22:57:12 -0700

    Adds unsplash API support

--- frontend/src/components/UnsplashImage.vue
+++ frontend/src/components/UnsplashImage.vue
@@ -1,30 +1,48 @@
 <template>
-    <img src="https://source.unsplash.com/random" />
+    <div class="unsplash-image-container">
+      <img v-bind:src="imageUrl" />
+    </div>
 </template>
 
 <script>
+const axios = require('axios').default;
+
 export default {
   name: 'UnsplashImage',
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      imageUrl: '',
+      loading: false,
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  watch: {
+    $route: 'fetchData',
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      const self = this;
+      axios.get('http://localhost:3001/unsplash')
+        .then((response) => {
+          self.loading = false;
+          self.imageUrl = response.data.urls.full;
+        });
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.unsplash-image-container {
+  img {
+    max-width: 100%;
+  }
 }
 </style>
