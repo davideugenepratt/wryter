17de7248898c7d0d5af786018c1f5e747e7aef6f
Cassidy Tillemans <cassidytillemans@Cassidys-MacBook-Pro.local>
2020-04-14 22:54:01 -0700

    added wryterBox text area with word count feature

---
+++ frontend/src/components/WryterBox.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="wryterBox-container">
+    <textarea name="" id="" cols="200" rows="10" style="resize:none"
+    v-model='input' and @keyup='countCharacters()'></textarea>
+    <br/>
+     <span>{{ wordCount }} / {{wordGoal}}</span>
+     <br>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: '',
+      wordCount: 0,
+      wordGoal: 250,
+    };
+  },
+  methods: {
+    countCharacters() {
+      this.wordCount = this.input.match(/\w+/g).length;
+    },
+  },
+};
+</script>
