4fbf4a841de34114a6cc91ff59f0a7b7669e37d7
Cassidy Tillemans <cassidytillemans@Cassidys-MacBook-Pro.local>
2020-04-16 23:41:06 -0700

    adds register route

---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <div class="text-center">
+    <form class="form-signin">
+      <h1 class="mb-3"></h1>
+      <label for="inputEmail" class="sr-only">Email Address</label>
+      <input type="email" id="inputEmail" class="form-control"
+      placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only ">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password">
+    </form>
+  </div>
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Register',
+};
+</script>
