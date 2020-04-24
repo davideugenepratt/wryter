602035f71a71a49f5a1dcc3dfb4d8f4cb4858321
Cassidy Tillemans <cassidytillemans@Cassidys-MacBook-Pro.local>
2020-04-23 17:22:05 -0700

    adds progress bar

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,10 @@
 var mongoose = require('mongoose');
-//  mongoose.connect('mongodb://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_HOST + '@' + process.env.MONGO_PASSWORD + ':' + process.env.MONGO_PORT + '/' + process.env.MONGO_DATABASE , function(){});
-mongoose.connect('mongodb://127.0.0.1:27017/Wryter');
+
+
+let authString = process.env.MONGO_USERNAME && process.env.MONGO_PASSWORD ? process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + '@' : '';
+
+mongoose.connect('mongodb://' + authString + process.env.MONGO_HOST + ':' + process.env.MONGO_PORT + '/' + process.env.MONGO_DATABASE , function(){
+    console.log(authString);
+});
+
 module.exports = mongoose;
