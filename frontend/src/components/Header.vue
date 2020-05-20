fbc1a76158e954adddcce5a3470f9bc3f9d101f8
David Pratt <david@davideugenepratt.com>
2020-05-19 23:00:57 -0700

    Adds malory theme

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,4 +1,80 @@
 <template>
+  <div class="navbar navbar-default default" role="navigation">
+    <div class="navbar-header">
+      <div class="navbar-brand">
+        <a href="/">
+          <img src="#" srcset="style/images/logo.png 1x, style/images/logo@2x.png 2x" alt="" />
+        </a>
+      </div>
+      <div class="nav-bars-wrapper">
+        <div class="nav-bars-inner">
+          <div class="nav-bars" data-toggle="collapse" data-target=".navbar-collapse">
+            <span></span>
+          </div>
+        </div>
+      </div>
+      <!-- Hiding this for now but we'll want to add these back in another story -->
+      <!--
+      <div class="header-other">
+        <div class="btn-group pull-right text-right">
+          <a href="#" class="share-button btn btn-light dropdown-toggle" data-toggle="dropdown">
+            <span></span>
+            Share
+          </a>
+          <div class="dropdown-menu">
+            <div class="goodshare-wrapper">
+              <a href="#" class="goodshare" data-type="fb">Share</a>
+              <a href="#" class="goodshare" data-type="tw">Tweet</a>
+              <a href="#" class="goodshare" data-type="pt">Pin</a>
+              <a href="#" class="goodshare" data-type="tm">Post</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      -->
+    </div>
+    <div class="nav-wrapper">
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <!--
+          <li><a href="#!">Portfolio <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="index.html">Classic Portfolio</a></li>
+              <li><a href="index2.html">Fullscreen Gallery</a></li>
+              <li><a href="index3.html">Lightbox Portfolio</a></li>
+                <li><a href="index4.html">Flow Gallery</a></li>
+            </ul>
+          </li>
+          <li><a href="#!">Pages <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="about.html">About</a></li>
+              <li><a href="services.html">Services</a></li>
+            </ul>
+          </li>
+          <li><a href="#!">Blog <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="blog.html">Tile Blog</a></li>
+              <li><a href="blog2.html">Classic Blog</a></li>
+              <li><a href="blog-post.html">Blog Post</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.html">Contact</a></li>
+          -->
+          <li><a href="#!">Features <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="elements.html">Elements</a></li>
+              <li><a href="headings.html">Headings</a></li>
+              <li><a href="pricing.html">Pricing Tables</a></li>
+              <li><a href="disqus.html">Disqus</a></li>
+              <li><a href="icon-budicon.html">Budicon Icons</a></li>
+              <li><a href="icon-fontello.html">Fontello Icons</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!--
   <header>
     <div class="navbar navbar-dark bg-dark box-shadow">
       <div class="container d-flex justify-content-between">
@@ -10,6 +86,7 @@
       </div>
     </div>
   </header>
+  -->
 </template>
 
 <script>
