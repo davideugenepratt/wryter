a1d31b4358e29bc571b079a3770c10d0e0a6d48c
David Pratt <david@davideugenepratt.com>
2021-01-24 16:48:04 -0800

    Fixes build by switching to yarn

--- frontend/src/components/UnsplashImage.vue
+++ frontend/src/components/UnsplashImage.vue
@@ -85,7 +85,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-
 .unsplash-image-container {
   position: relative;
   height: 100vh;
